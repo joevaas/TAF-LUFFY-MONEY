ff063f835e8f2d98f1cb8bb1710c9a02937f0366
joevaas <133934323+joevaas@users.noreply.github.com>
2024-11-27 22:00:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6966079567:AAEMZs_kK9VwPWXbZfjW6kpYq1Yv4TiRpbo")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6749506390:AAE6lu6YuZEekzFmuv9sIxtZ3mPV1ETFQII")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "28264594"))
@@ -16,27 +16,27 @@ APP_ID = int(os.environ.get("APP_ID", "28264594"))
 API_HASH = os.environ.get("API_HASH", "94ca8a089020a2290fd29a41f18acb94")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001995978690"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002114522672"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6497757690"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6693549185"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://publicdb:publicdb@cluster0.aisg0rh.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "codeflix")
+DB_NAME = os.environ.get("DATABASE_NAME", "taf_luffy")
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "1d74269ab2c3349995cd15776033eea192df8327")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "10")) # Add time in seconds
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "instantearn.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "ae7c5805f074b71840d10f5983edbf2bb430743c")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "28800")) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/Anime_Cruise_Netflix")
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/True_Beauty_Tamil_Dub_Anime/48")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001495022147"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1001723817903"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002005092018"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002093430780"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
@@ -45,7 +45,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6497757690").split()):
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
